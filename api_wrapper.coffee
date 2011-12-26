b692105516ca71da66c1de9a4983bbacb8c5ff99
Wojciech Zieliński <zielinskiwojciech@gmail.com>
2011-12-26 16:49:46 +0100

    a

--- api_wrapper.coffee
+++ api_wrapper.coffee
@@ -1,3 +1,29 @@
+class Viewer3d
+  constructor: ->
+    @canvas = $('#preview3d')[0]
+    
+  place: ->
+    @viewer = new JSC3D.Viewer(@canvas)
+  
+  setParams: ->
+    @viewer.setParameter('InitRotationX', 0)
+    @viewer.setParameter('InitRotationY', 45)
+    @viewer.setParameter('InitRotationZ', 0)
+    @viewer.setParameter('ModelColor', '#FFFFFF')
+    @viewer.setParameter('SimulateSpecular', 'true')
+    @viewer.setParameter('BackgroundColor1', '#F4F4F4')
+    @viewer.setParameter('BackgroundColor2', '#ECECEC')
+    @viewer.setParameter('RenderMode', 'texturesmooth')
+    @viewer.setParameter('Definition', 'low')
+    @viewer.setParameter('MipMapping', 'on')
+
+  loadScene: (url)->
+    @viewer.setParameter('SceneUrl', url);
+  
+  refresh: ->
+    @viewer.init()
+    @viewer.update()
+
 class Layout
   constructor: ->
     @api_url = "https://api.github.com/repos/baael/czyde/"
@@ -5,29 +31,79 @@ class Layout
     @raw_url = "https://raw.github.com/Baael/czyde/"+@branch+"/preview/"
     @row = '<tr><td><a id="{path}" href="'+@raw_url+'{path}"><img src="'+@raw_url+'{path}/icon.png"/>{path}</td><td></td></tr>'
     @preview = '<img src="{path}/big_icon.png"><h1>{title}</h1><div class="author">by {author}</div>'
-    @dl = '<dl><dd>forum profile:</dd><dt>{forum_profile}</dt><dd>polygons count:</dd><dt>{polygons}</dt><dd>shader:</dd><dt>{shader}</dt><dd>FO3D included:</dd><dt>{fo3d}</dt><dd>rigged:</dd><dt>{rigged}</dt><dd>category:</dd><dt>{category}</dt></dl><div id="description_text">{description}</div>'
+    @dl = '<dl><dd>forum profile:</dd><dt>{forum_profile}</dt><dd>polygons count:</dd><dt>{polygons}</dt><dd>shader:</dd><dt>{shader}</dt><dd>FO3D included:</dd><dt>{fo3d}</dt><dd>rigged:</dd><dt>{rigged}</dt><dd>category:</dd><dt>{category}</dt></dl><div id="description_text" class="non3d">{description}</div>'
     @listBindings()
+    @list = {}
+    @global_req
 
   getCurrentHash: ->
     self = @
     @getJSONP @api_url+'git/trees/master', (data) ->
-      self.sha = item.sha for item in data.data.tree when item.path is "preview"  
+      tree = {}
+      tree = item for item in data.data.tree when item.path is "preview"
+      self.sha = tree[0].sha
+      self.tree_url = tree[0].url
+      self.getObjectsList()
       self.listPreview()
+
+  getObjectsList: ->
+    self = @
+    @getJSONP self.tree_url, (data) ->
+      self.assignObjectDirectory(item) for item in data.data.tree when item.type is "tree"
+
+  assignObjectDirectory: (obj)->
+    self = @
+    @list[obj.path] = obj
+    @list[obj.path].items = {}
+    @getJSONP obj.url, (data) ->
+      self.assignObjectTree(obj.path, item) for item in data.data.tree
+    console.log(@list)
+    
+
+  getAndDecode: (url, callback) ->
+    self = @
+    @getJSONP url, (data) ->
+      callback($.base64Decode(data.data.content))
+      
+
+  assignObjectTree: (enumerator, obj)->
+    @list[enumerator].items[obj.path] = obj 
 
   fetchData: ->
     @getCurrentHash()
 
+  previewBindings: ->
+    $('#buttons a').live 'click', (e)->
+      if ($('body').hasClass('in3d'))
+        if ($(this).hasClass('togglable'))
+          $('.togglable').removeClass('turned_on')
+        $(this).addClass('turned_on')
+      e.preventDefault()
+      false
+    $('#run3d').live 'click', (e)->
+      $('body').toggleClass('in3d')
+      if ($('body').hasClass('in3d') == false)
+        $('.turned_on').removeClass('turned_on')
+      else
+        $(this).addClass('turned_on')
+      
   listBindings: ->
     self = @
+    @previewBindings()
     $('#list a').live 'click', (e)->
+      $('.selected').removeClass('selected')
+      $(this).parents('tr').addClass('selected')
       e.preventDefault()
-      enumerator = $(this).attr('id')
-      element = details[enumerator]
-      element.path = self.raw_url+enumerator
       
-      
-      $('#preview').html($.nano(self.preview, element))
-      $('#text').html($.nano(self.dl, element))
+      #element = details[enumerator]
+      #element.path = self.raw_url+enumerator
+      item = self.list[$(this).attr('id')]
+      console.log(item.items['author.json'].url)
+      self.getAndDecode item.items['author.json'].url, (content) ->
+        console.log(content)
+      #self.getJSONP item 
+      #$('#preview').html($.nano(self.preview, element))
+      #$('#text').html($.nano(self.dl, element))
       
       false
 
@@ -56,13 +132,24 @@ class Layout
 $(document).ready ->
   window.details = {}
   jQuery.support.cors = false
-  layout = new Layout()
-  layout.fetchData()
+  window.layout = new Layout()
+  window.layout.fetchData()
   
-  window.req = $.ajax {
-    url: 'https://raw.github.com/Baael/czyde/master/preview/jacket/author.json'
-    type: 'GET'
-    dataType: 'jsonp'
-    success : (xhr,data,b)->
-      console.log(xhr)
-    }
+  window.viewer = new Viewer3d()
+  #viewer.loadScene('https://raw.github.com/Baael/czyde/master/preview/jacket/object.obj') 
+  
+  #window.req = $.ajax {
+  #  url: 'https://raw.github.com/Baael/czyde/master/preview/jacket/author.json'
+  #  type: 'GET'
+  #  dataType: 'jsonp'
+  #  success : (xhr,data,b)->
+  #    console.log(xhr)
+  #  }
+    
+  #window.req = $.ajax {
+  #  url: 'https://raw.github.com/Baael/czyde/master/preview/jacket/object.obj'
+  #  type: 'GET'
+  #  dataType: 'jsonp'
+  #  success : (xhr,data,b)->
+  #    console.log(xhr)
+  #  }
--- api_wrapper.js
+++ api_wrapper.js
@@ -1,45 +1,145 @@
 (function(){
-  var Layout;
+  var Layout, Viewer3d;
+  Viewer3d = function() {
+    this.canvas = $('#preview3d')[0];
+    return this;
+  };
+  Viewer3d.prototype.place = function() {
+    this.viewer = new JSC3D.Viewer(this.canvas);
+    return this.viewer;
+  };
+  Viewer3d.prototype.setParams = function() {
+    this.viewer.setParameter('InitRotationX', 0);
+    this.viewer.setParameter('InitRotationY', 45);
+    this.viewer.setParameter('InitRotationZ', 0);
+    this.viewer.setParameter('ModelColor', '#FFFFFF');
+    this.viewer.setParameter('SimulateSpecular', 'true');
+    this.viewer.setParameter('BackgroundColor1', '#F4F4F4');
+    this.viewer.setParameter('BackgroundColor2', '#ECECEC');
+    this.viewer.setParameter('RenderMode', 'texturesmooth');
+    this.viewer.setParameter('Definition', 'low');
+    return this.viewer.setParameter('MipMapping', 'on');
+  };
+  Viewer3d.prototype.loadScene = function(url) {
+    return this.viewer.setParameter('SceneUrl', url);
+  };
+  Viewer3d.prototype.refresh = function() {
+    this.viewer.init();
+    return this.viewer.update();
+  };
+
   Layout = function() {
     this.api_url = "https://api.github.com/repos/baael/czyde/";
     this.branch = "master";
     this.raw_url = "https://raw.github.com/Baael/czyde/" + this.branch + "/preview/";
     this.row = '<tr><td><a id="{path}" href="' + this.raw_url + '{path}"><img src="' + this.raw_url + '{path}/icon.png"/>{path}</td><td></td></tr>';
     this.preview = '<img src="{path}/big_icon.png"><h1>{title}</h1><div class="author">by {author}</div>';
-    this.dl = '<dl><dd>forum profile:</dd><dt>{forum_profile}</dt><dd>polygons count:</dd><dt>{polygons}</dt><dd>shader:</dd><dt>{shader}</dt><dd>FO3D included:</dd><dt>{fo3d}</dt><dd>rigged:</dd><dt>{rigged}</dt><dd>category:</dd><dt>{category}</dt></dl><div id="description_text">{description}</div>';
+    this.dl = '<dl><dd>forum profile:</dd><dt>{forum_profile}</dt><dd>polygons count:</dd><dt>{polygons}</dt><dd>shader:</dd><dt>{shader}</dt><dd>FO3D included:</dd><dt>{fo3d}</dt><dd>rigged:</dd><dt>{rigged}</dt><dd>category:</dd><dt>{category}</dt></dl><div id="description_text" class="non3d">{description}</div>';
     this.listBindings();
+    this.list = {};
+    this.global_req;
     return this;
   };
   Layout.prototype.getCurrentHash = function() {
     var self;
     self = this;
     return this.getJSONP(this.api_url + 'git/trees/master', function(data) {
-      var _a, _b, _c, _d, item;
-      self.sha = (function() {
+      var _a, _b, _c, _d, item, tree;
+      tree = {};
+      tree = (function() {
         _a = []; _c = data.data.tree;
         for (_b = 0, _d = _c.length; _b < _d; _b++) {
           item = _c[_b];
-          item.path === "preview" ? _a.push(item.sha) : null;
+          item.path === "preview" ? _a.push(item) : null;
         }
         return _a;
       })();
+      self.sha = tree[0].sha;
+      self.tree_url = tree[0].url;
+      self.getObjectsList();
       return self.listPreview();
     });
+  };
+  Layout.prototype.getObjectsList = function() {
+    var self;
+    self = this;
+    return this.getJSONP(self.tree_url, function(data) {
+      var _a, _b, _c, _d, item;
+      _a = []; _c = data.data.tree;
+      for (_b = 0, _d = _c.length; _b < _d; _b++) {
+        item = _c[_b];
+        item.type === "tree" ? _a.push(self.assignObjectDirectory(item)) : null;
+      }
+      return _a;
+    });
+  };
+  Layout.prototype.assignObjectDirectory = function(obj) {
+    var self;
+    self = this;
+    this.list[obj.path] = obj;
+    this.list[obj.path].items = {};
+    this.getJSONP(obj.url, function(data) {
+      var _a, _b, _c, _d, item;
+      _a = []; _c = data.data.tree;
+      for (_b = 0, _d = _c.length; _b < _d; _b++) {
+        item = _c[_b];
+        _a.push(self.assignObjectTree(obj.path, item));
+      }
+      return _a;
+    });
+    return console.log(this.list);
+  };
+  Layout.prototype.getAndDecode = function(url, callback) {
+    var self;
+    self = this;
+    return this.getJSONP(url, function(data) {
+      return callback($.base64Decode(data.data.content));
+    });
+  };
+  Layout.prototype.assignObjectTree = function(enumerator, obj) {
+    this.list[enumerator].items[obj.path] = obj;
+    return this.list[enumerator].items[obj.path];
   };
   Layout.prototype.fetchData = function() {
     return this.getCurrentHash();
   };
+  Layout.prototype.previewBindings = function() {
+    $('#buttons a').live('click', function(e) {
+      if (($('body').hasClass('in3d'))) {
+        ($(this).hasClass('togglable')) ? $('.togglable').removeClass('turned_on') : null;
+        $(this).addClass('turned_on');
+      }
+      e.preventDefault();
+      return false;
+    });
+    return $('#run3d').live('click', function(e) {
+      $('body').toggleClass('in3d');
+      if (($('body').hasClass('in3d') === false)) {
+        return $('.turned_on').removeClass('turned_on');
+      } else {
+        return $(this).addClass('turned_on');
+      }
+    });
+  };
   Layout.prototype.listBindings = function() {
     var self;
     self = this;
+    this.previewBindings();
     return $('#list a').live('click', function(e) {
-      var element, enumerator;
+      var item;
+      $('.selected').removeClass('selected');
+      $(this).parents('tr').addClass('selected');
       e.preventDefault();
-      enumerator = $(this).attr('id');
-      element = details[enumerator];
-      element.path = self.raw_url + enumerator;
-      $('#preview').html($.nano(self.preview, element));
-      $('#text').html($.nano(self.dl, element));
+      //element = details[enumerator]
+      //element.path = self.raw_url+enumerator
+      item = self.list[$(this).attr('id')];
+      console.log(item.items['author.json'].url);
+      self.getAndDecode(item.items['author.json'].url, function(content) {
+        return console.log(content);
+        //self.getJSONP item
+        //$('#preview').html($.nano(self.preview, element))
+        //$('#text').html($.nano(self.dl, element))
+      });
       return false;
     });
   };
@@ -74,19 +174,25 @@
   };
 
   $(document).ready(function() {
-    var layout;
     window.details = {};
     jQuery.support.cors = false;
-    layout = new Layout();
-    layout.fetchData();
-    window.req = $.ajax({
-      url: 'https://raw.github.com/Baael/czyde/master/preview/jacket/author.json',
-      type: 'GET',
-      dataType: 'jsonp',
-      success: function(xhr, data, b) {
-        return console.log(xhr);
-      }
-    });
-    return window.req;
+    window.layout = new Layout();
+    window.layout.fetchData();
+    window.viewer = new Viewer3d();
+    //viewer.loadScene('https://raw.github.com/Baael/czyde/master/preview/jacket/object.obj')
+    //window.req = $.ajax {
+    //  url: 'https://raw.github.com/Baael/czyde/master/preview/jacket/author.json'
+    //  type: 'GET'
+    //  dataType: 'jsonp'
+    //  success : (xhr,data,b)->
+    //    console.log(xhr)
+    //  }
+    //window.req = $.ajax {
+    //  url: 'https://raw.github.com/Baael/czyde/master/preview/jacket/object.obj'
+    //  type: 'GET'
+    //  dataType: 'jsonp'
+    //  success : (xhr,data,b)->
+    //    console.log(xhr)
+    //  }
   });
 })();
